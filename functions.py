7ed0ccbb908a395e1e492bcb27a375ebe5a669f5
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-02-08 21:49:46 -0500

    upd

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ from pyfiglet import figlet_format
 
 
 def print_image(text):
-       """
+    """
        Based on https://github.com/pwaller/pyfiglet
 
        :param text: if not string, - converts value to string and processes it into ASCII-art.
@@ -16,13 +16,42 @@ def print_image(text):
     result = print(figlet_format(str(text), font='graceful'))
     return result
 
-rock_image = print_image("Rock!")
-paper_image = print_image("Paper!")
-scissors_image = print_image("Scissors!")
-lizard_image = print_image("Lizard!")
-spock_image = print_image("Spock!")
-fire_image = print_image("Fire!")
-water_image = print_image("Water!")
+
+def game_logo(game_type=1):
+    """
+       Simple logo creator
+       :param game_type: one of three game types
+       :return: print iteration on the game type
+    """
+    game_logo = {
+        1: (
+            "Rock!",
+            "Paper!",
+            "Scissors!"
+        ),
+        2: (
+            "Rock!",
+            "Paper!",
+            "Scissors!",
+            "Spock!",
+            "Lizard!",
+        ),
+        3: (
+            "Rock!",
+            "Paper!",
+            "Scissors!",
+            "Fire!",
+            "Water!",
+        )
+
+    }
+    for k,v in game_logo.items():
+           try:
+                  if game_type == k:
+                         return [print_image(x) for x in v]
+
+           except:
+                  return [print_image(x) for x in game_logo[1]]
 
 
 
