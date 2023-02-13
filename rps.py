50cd4692e0607f04bed9092d8cf3d0ef1853fbfb
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-02-13 12:24:47 -0500

    upd

--- rps.py
+++ rps.py
@@ -2,31 +2,12 @@ import sys
 import time
 from random import randrange
 
-##test test
 from colorama import init
 
 init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
 from pyfiglet import figlet_format
 
-
-def print_image(text):
-    """
-       Based on https://github.com/pwaller/pyfiglet
-
-       :param text: if not string, - converts value to string and processes it into ASCII-art.
-       :return: ASCII-art printed text
-       """
-    result = print(figlet_format(str(text), font='graceful'))
-    return result
-
-
-def game_logo(game_type=1):
-    """
-       Simple logo creator
-       :param game_type: one of three game types
-       :return: print iteration on the game type
-    """
-    game_logo = {
+game_logo = {
         1: (
             "Rock!",
             "Paper!",
@@ -48,6 +29,27 @@ def game_logo(game_type=1):
         )
 
     }
+
+
+
+def print_image(text):
+    """
+       Based on https://github.com/pwaller/pyfiglet
+
+       :param text: if not string, - converts value to string and processes it into ASCII-art.
+       :return: ASCII-art printed text
+       """
+    result = print(figlet_format(str(text), font='graceful'))
+    return result
+
+
+def game_logo(game_type=1):
+    """
+       Simple logo creator
+       :param game_type: one of three game types
+       :return: print iteration on the game type
+    """
+
     for k,v in game_logo.items():
            try:
                   if game_type == k:
