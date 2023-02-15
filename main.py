42621e146081ce3f50511a71543771051d3c4dd3
Roman Vlasenko <klavionik@gmail.com>
2023-02-15 23:25:25 +0300

    Add comments

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+from rps import (
+    comp_player,
+    delimiter,
+    game_logo,
+    game_options,
+    print_image,
+    screener,
+    show_board,
+    win_checker,
+)
 
-from rps import game_logo, screener, delimiter, show_board, game_options, comp_player, win_checker, print_image
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(screener("  Hello, stranger.  "))
     print(screener("  Please, enter your name  "))
     delimiter()
+    # TODO: можно ввести пустое значение
     name = input("Enter your name:\n")
-    score_board = {name:0, "Computer":0}
+    score_board = {name: 0, "Computer": 0}
     delimiter()
     while True:
+        # TODO: позасрал все своими принтами!
         print(screener(f"  Hello, {name}.  "))
         delimiter()
         print(screener("  What game would you like to play?  "))
@@ -18,24 +27,26 @@ if __name__ == '__main__':
         print(screener("  3 - RPS-Fire-Water  --"))
         print(screener("  other - to quit  -----"))
         delimiter()
+        # TODO: нет валидации ввода
         game_type = int(input("Game type is: \n"))
-        if game_type in {1,2,3}:
-                delimiter()
-                game_logo(game_type)
+        if game_type in {1, 2, 3}:
+            delimiter()
+            game_logo(game_type)
         else:
-                delimiter()
-                break
-                print(screener("Thank you for playing"))
-
+            delimiter()
+            break
+            print(screener("Thank you for playing"))
 
         while True:
-
             show_board(score_board)
 
-            print(screener('make your choice:'))
+            print(screener("make your choice:"))
 
             choices_available = {}
             n = 1
+            # TODO: вынести в функцию
+            # TODO: использовать enumerate
+            # TODO: нормально назвать переменные
             for x in game_options[game_type]:
                 choices_available[n] = x
                 return_value = f" {n} - {x}  "
@@ -43,10 +54,13 @@ if __name__ == '__main__':
                 n += 1
 
             try:
+                # TODO: не надо так много заворачивать в try/except
                 choice = int(input("and your choice is:  "))
                 if 1 <= choice <= len(game_options[game_type]):
                     comp_choice = comp_player(game_type)
-                    x = win_checker(player1=choice, player2=comp_choice, game_type=game_type)
+                    x = win_checker(
+                        player1=choice, player2=comp_choice, game_type=game_type
+                    )
                     print(f"your choice was:")
                     print_image(choices_available[choice])
                     print(f"Computer chose:")
@@ -65,13 +79,6 @@ if __name__ == '__main__':
                 else:
                     raise ValueError
             except ValueError:
+                # TODO: я сначала подумал, что это про уровни в игре какие-то :)
                 print("Going one level up!")
                 break
-
-
-
-
-
-
-
-
--- rps.py
+++ rps.py
@@ -8,71 +8,65 @@ init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
 from pyfiglet import figlet_format
 
 game_options = {
-        1: (
-            "Rock!",
-            "Paper!",
-            "Scissors!"
-        ),
-        2: (
-            "Rock!",
-            "Paper!",
-            "Scissors!",
-            "Spock!",
-            "Lizard!",
-        ),
-        3: (
-            "Rock!",
-            "Paper!",
-            "Scissors!",
-            "Fire!",
-            "Water!",
-        )
-
-    }
-
+    1: ("Rock!", "Paper!", "Scissors!"),
+    2: (
+        "Rock!",
+        "Paper!",
+        "Scissors!",
+        "Spock!",
+        "Lizard!",
+    ),
+    3: (
+        "Rock!",
+        "Paper!",
+        "Scissors!",
+        "Fire!",
+        "Water!",
+    ),
+}
 
 
 def print_image(text):
     """
+    # TODO: писать "based on" со ссылкой на гитхаб не надо, видно же что используется функция из библиотеки
        Based on https://github.com/pwaller/pyfiglet
 
        :param text: if not string, - converts value to string and processes it into ASCII-art.
        :return: ASCII-art printed text
-       """
-    result = print(figlet_format(str(text), font='graceful'))
+    """
+    # TODO: возвращать результат print не нужно
+    result = print(figlet_format(str(text), font="graceful"))
     return result
 
 
+# TODO: дефолтное значение тут не нужно
 def game_logo(game_type=1):
     """
-       Simple logo creator
-       :param game_type: one of three game types
-       :return: print iteration on the game type
+    Simple logo creator TODO: Неописательное описание
+    :param game_type: one of three game types
+    # TODO: Ну, на самом деле эта функция ничего не возвращает, поэтому :return: тут не нужен
+    :return: print iteration on the game type
     """
+    # TODO: функция делает очень много ненужного
+    for k, v in game_options.items():
+        try:
+            if game_type == k:
+                return [print_image(x) for x in v]
 
-    for k,v in game_options.items():
-           try:
-                  if game_type == k:
-                         return [print_image(x) for x in v]
-
-           except:
-                  return [print_image(x) for x in game_options[1]]
-
+        except:
+            return [print_image(x) for x in game_options[1]]
 
 
 def show_board(score_board):
-
     delimiter()
     for k, v in score_board.items():
+        # TODO: табуляция ломает скорборд
         print(screener(f"\t{k}                \t{v}\t"))
     delimiter()
 
 
-
-
-
-
-if __name__ == '__main__':
+# TODO: вот это явно надо было удолить
+if __name__ == "__main__":
     # rock_image = print_image("Rock!")
     # paper_image = print_image("Paper!")
     # scissors_image = print_image("Scissors!")
@@ -84,16 +78,22 @@ if __name__ == '__main__':
     game_logo(2)
 
 
+# TODO: параметр screen_limit не нужен, он нигде не используется
 def screener(data, screen_limit=40):
     """
+    # TODO: any type of data это конечно неправда
     :param data: any type of data that will be converted to string and returned
     :param screen_limit: the width of the screen so every line will look consistently. Default value = 40
     :return: type: str()
     """
+    # TODO: функция с багами
+    # TODO: вся функция заменяется на одну строку
+    # return f"{data:-^{width}}"
     margin = str("-" * int((screen_limit - len(data) / 2)))
+    # TODO: ???
     time.sleep(0.01)
     if len(data) <= screen_limit:
-        string = (margin + str(data) + margin)
+        string = margin + str(data) + margin
     else:
         string = margin + str(data[:screen_limit]) + margin
 
@@ -110,8 +110,11 @@ def comp_player(game_type):
     """
 
     reply = None
+    # TODO: вместо условий и неявной зависимости лучше было бы передавать в функцию список опций
+    # TODO: передавать в эту функцию количество опций и заменить дереве if-else на один вызов
     if game_type == 1:
         reply = randrange(1, 4, 1)
+    # TODO: баг, крашится при выборе 2-го типа игры
     elif game_type == 2 | 3:
         reply = randrange(1, 6, 1)
     else:
@@ -121,6 +124,7 @@ def comp_player(game_type):
 
 def win_checker(player1, player2, game_type):
     """
+    # TODO: вот тут очень пригодилось бы описание алгоритма
     :param player1: one of the positions (1-3 or 1-5)
     :param player2: one of the positions (1-3 or 1-5)
     :param game_type:
@@ -133,29 +137,30 @@ def win_checker(player1, player2, game_type):
      None = if data is incorrect
     """
     try:
+        # TODO: вынести scoreboard в словарь как с game_options
+        # TODO: кстати, а почему это называется scoreboard?
         if game_type == 1:
             # 1-Rock, 2-Paper, 3-Scissors in rows and columns
             # rock,paper,scissors
-            scoreboard = [(0, 1, 2),  # rock
-                          (2, 0, 1),  # paper
-                          (1, 2, 0)  # scissors
-                          ]
+            scoreboard = [(0, 1, 2), (2, 0, 1), (1, 2, 0)]  # rock  # paper  # scissors
         elif game_type == 2:
             # rock,paper,scissors,spock,lizard
-            scoreboard = [(0, 1, 2, 1, 2),  # rock
-                          (2, 0, 1, 2, 1),  # paper
-                          (1, 2, 0, 1, 2),  # scissors
-                          (2, 1, 2, 0, 1),  # spock
-                          (1, 2, 1, 2, 0)  # lizard
-                          ]
+            scoreboard = [
+                (0, 1, 2, 1, 2),  # rock
+                (2, 0, 1, 2, 1),  # paper
+                (1, 2, 0, 1, 2),  # scissors
+                (2, 1, 2, 0, 1),  # spock
+                (1, 2, 1, 2, 0),  # lizard
+            ]
         elif game_type == 3:
             # rock,paper,scissors,fire,  water
-            scoreboard = [(0, 1, 2, 1, 2),  # rock
-                          (2, 0, 1, 1, 2),  # paper
-                          (1, 2, 0, 1, 2),  # scissors
-                          (2, 2, 2, 0, 1),  # fire
-                          (1, 1, 1, 2, 0)  # Water
-                          ]
+            scoreboard = [
+                (0, 1, 2, 1, 2),  # rock
+                (2, 0, 1, 1, 2),  # paper
+                (1, 2, 0, 1, 2),  # scissors
+                (2, 2, 2, 0, 1),  # fire
+                (1, 1, 1, 2, 0),  # Water
+            ]
         else:
             print(f"Wrong game type! you've entered {game_type}")
             return None
@@ -168,6 +173,6 @@ def win_checker(player1, player2, game_type):
 
 
 def delimiter():
-        for x in range(3):
-            print(screener("  "))
-        return
+    for x in range(3):
+        print(screener("  "))
+    return
